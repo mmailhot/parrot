2fb1bf0198feb2aa188899733269d24abc5658e7
Marc Mailhot <mmm11105@gmail.com>
2014-09-20 18:08:49 -0400

    Added MASTER READ FUNCTION

--- app/scripts/controllers/emaildisplay.js
+++ app/scripts/controllers/emaildisplay.js
@@ -8,13 +8,17 @@
  * Controller of the hackathon2App
  */
 angular.module('voicemailApp')
-  .controller('EmaildisplayCtrl', function ($scope, $filter, gmailService) {
+  .controller('EmaildisplayCtrl', function ($scope, $filter, $q, gmailService, witService) {
 
     $('#first-time-modal').modal();
 
     $scope.emails = [];
 
-    gmailService.getEmails().then(function (data) {
+    gmailService.getEmails().t$scope.speech('Email #' + (index + 1));
+      $scope.speech('From ' + email.sender);
+      $scope.speech('The subject is ' + email.subject);
+      $scope.speech(email.text);
+      $scope.lastRead = index;hen(function (data) {
         for (var i = 0; i < data.messages.length; i++) {
             gmailService.getEmail(data.messages[i].id).then(function(e) {
                 $scope.emails.push(e);
@@ -27,16 +31,32 @@ angular.module('voicemailApp')
     $scope.lastRead = -1;
 
     $scope.speech = function speech(text) {
+      var defer = $q.defer();
       text = new SpeechSynthesisUtterance(text);
+      text.onend = defer.resolve;
       window.speechSynthesis.speak(text);
+
+      return defer.promise;
     };
 
-    $scope.read = function read(email, index) {
-      $scope.speech('Email #' + (index + 1));
-      $scope.speech('From ' + email.sender);
-      $scope.speech('The subject is ' + email.subject);
-      $scope.speech(email.text);
-      $scope.lastRead = index;
+    $scope.readHeaders = function read(email, index) {
+      var defer = $q.defer();
+      $scope.speech('Email ' + (index + 1)).then(function()){
+        $scope.speech('The subject is ' + email.subject).then(function(){
+          $scope.speech('From ' + email.sender).then(function(){
+            q.resolve();
+          });
+        });
+      });
+      return defer.promise;
+    };
+
+    $scope.readBody = function read(email) {
+      var defer = $q.defer();
+      $scope.speech(email.body).then(function()){
+        q.resolve();
+      });
+      return defer.promise;
     };
 
     $scope.readPrevious = function readPrevious() {
@@ -56,4 +76,46 @@ angular.module('voicemailApp')
     $scope.stopReading = function stopReading() {
       window.speechSynthesis.cancel();
     };
+
+    $scope.readAll = function() {
+      $scope.readFrom(0);
+    }
+
+    $scope.readFrom = function(id){
+      if(id < emails.length()){
+        $scope.readHeaders(emails[id],id).then(function() {
+          $scope.parseSpeechFunction(id);
+        });
+      }else{
+        var intent = witService.recognize().then(function(intent){
+          if(intent == "finish"){
+            $scope.speech("OK. Have a nice day!");
+          }else{
+            $scope.readFrom(id);
+          }
+        }):
+      }
+    }
+
+    $scope.parseSpeechFunction = function(id){
+      var intent = witService.recognize().then(function(intent){
+        if(intent == "read"){
+          $scope.readBody(emails[id]);
+          $scope.parseSpeechFunction(id);
+        }else if(intent == "repeat"){
+          $scope.readFrom(id);
+        }else if(intent == "finish"){
+          $scope.speech("OK. Have a nice day!");
+        }else if(intent == "flag"){
+          $scope.flag(id);
+          $scope.parseSpeechFunction(id);
+        }else{
+          $scope.readFrom(id + 1s);
+        }
+      });
+    }
+
+    $scope.flag = function(id){
+      console.log("Email #" + id " flagged!");
+    }
   });
