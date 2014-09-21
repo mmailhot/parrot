a6f41a94dce0a542dbc181771c97ddf1d5bced0b
Marc Mailhot <mmm11105@gmail.com>
2014-09-20 21:04:53 -0400

    Fixed server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname + "/app"));
 
-var port = process.env.PORT | 8080;
+var port = process.env.PORT || 8080;
 
 console.log(port);
 app.listen(port);
